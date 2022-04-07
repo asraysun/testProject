e6370f337b7c01432d53115b4a60436d8a75897f
andrey zapolskiy <uniqo@yandex.by>
2022-04-07 20:53:47 +0300

    change Name

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi("Nick")
+    print_hi("Mike")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
